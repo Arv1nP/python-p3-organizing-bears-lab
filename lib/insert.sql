8d4794f415591629b9d8bbd23cf7f05d0181071f
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2024-01-04 16:20:19 +1100

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- Inserting 8 bears
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+('Tabitha', 2, 'F', 'Gray', 'Calm', 1),
+('Sergeant Brown', 7, 'M', 'Brown', 'Disciplined', 1),
+('Melissa', 4, 'F', 'White', 'Playful', 1),
+('Grinch', 6, 'M', 'Green', 'Mischievous', 1),
+('Wendy', 8, 'F', 'Golden', 'Sweet', 1),
+(NULL, NULL, NULL, NULL, NULL, NULL);
